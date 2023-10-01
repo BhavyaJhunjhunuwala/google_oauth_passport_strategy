b098bcbb7074baed7babe6c7920304c82097eb14
Bhavya jhunjhunuwala <bhavyajhunjhunuwala@gmail.com>
2023-10-01 10:38:41 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect,useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [user,setUser]=useState(null);
+
+  const getUser=async()=>{
+    try{
+        const url='http://localhost:8080/auth/login/success';
+        const data=await axios.get(url,{withCredentials:true});
+        console.log(data.user._json);
+    }catch(err){
+        console.log(err);
+    }
+  }
+
+  useEffect(()=>{
+    getUser();
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hello world
     </div>
   );
 }
